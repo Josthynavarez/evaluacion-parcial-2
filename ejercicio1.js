a1628403840c0157784cd6facbf5e99089ce8f86
Josthynavarez <Josthynalvarez1330@gmail.com>
2026-07-24 08:09:35 -0500

    Ejercicio 2: Variables y tipos de datos

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+const producto1 ={
+    nombre: "Laptop",
+    precio: 800,
+    stock: true
+};
+
+const producto2 ={
+    nombre: "Mouse",
+    precio: 25,
+    stock: false
+};
+
+console.log(producto1);
+console.log(producto2);
