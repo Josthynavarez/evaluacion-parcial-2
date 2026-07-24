c7919276dd424013d89d76acaa34e70bff3e6c5b
Josthynavarez <Josthynalvarez1330@gmail.com>
2026-07-24 08:46:55 -0500

    Ejercicio 6 Funciones

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,3 +1,4 @@
+//Ejercicio 2
 const producto1 ={
     nombre: "Laptop",
     precio: 800,
@@ -13,6 +14,7 @@ const producto2 ={
 console.log(producto1);
 console.log(producto2);
 
+//Ejercicio 3
 const precioOriginal = 100;
 const descuento = 0.15;
 const ahorro = precioOriginal * descuento;
@@ -21,6 +23,7 @@ const precioFinal = precioOriginal - ahorro;
 console.log("Precio final con descuento:", precioFinal);
 console.log("Dinero ahorrado:", ahorro);
 
+//Ejercicio 4
 function clasificarEdad(edad){
     if(edad <18){
         console.log("Menor de edad");
@@ -32,6 +35,7 @@ function clasificarEdad(edad){
 }
 clasificarEdad(25);
 
+//ejercicio 5
 let sumaTotal = 0;
 for (let i = 1; i <= 20; i ++){
     if (i % 2 === 0){
@@ -42,3 +46,14 @@ for (let i = 1; i <= 20; i ++){
     sumaTotal += i;
 }
 console.log ("LA suma total es:", sumaTotal);
+
+//ejercicio 6
+function obterMayor (num1, num2){
+    if (num1 > num2){
+        return num1;
+    }else{
+        return num2;
+    }
+}
+console.log("El numero mayor es:", obterMayor(50,85));
+
