419db68a8f21c79a04e5ff4adfe8b8230ab6dfd7
Josthynavarez <Josthynalvarez1330@gmail.com>
2026-07-24 08:20:00 -0500

    Ejercicio 3: Operadores

--- ejercicio1.js
+++ ejercicio1.js
@@ -12,3 +12,11 @@ const producto2 ={
 
 console.log(producto1);
 console.log(producto2);
+
+const precioOriginal = 100;
+const descuento = 0.15;
+const ahorro = precioOriginal * descuento;
+const precioFinal = precioOriginal - ahorro;
+
+console.log("Precio final con descuento:", precioFinal);
+console.log("Dinero ahorrado:", ahorro);
